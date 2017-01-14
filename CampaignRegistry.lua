de707e85d5794274a602d8da8cec528d925f179f
Kimusabi <thomas.hull89@gmail.com>
2017-01-15 07:51:53 +1030

    Pre-session planning 19/01/2017

--- CampaignRegistry.lua
+++ CampaignRegistry.lua
@@ -1,8 +1,9 @@
 {
 	["OptDDCL"] = "desktopdecal_ROD1",
+	["sidebar"] = "charsheet,note,image,table,story,quest,npc,battle,item,treasureparcel",
 	["OptTPCH"] = "dot",
-	["OptHRNH"] = "max",
 	["OptSHPW"] = "on",
+	["OptHRNH"] = "max",
 	["OptTBOX"] = "off",
 	["OptTFAC"] = "on",
 	["OptHRST"] = "off",
